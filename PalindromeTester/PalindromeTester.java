5da405ddcfc426a34a2b52e05e6b7a217bbcbeb5
neilshah22 <nnshah@stu.naperville203.org>
2016-03-17 15:08:44 -0500

    recursion

--- PalindromeTester/PalindromeTester.java
+++ PalindromeTester/PalindromeTester.java
@@ -21,26 +21,32 @@ public class PalindromeTester
       {
          System.out.println ("Enter a potential palindrome:");
          str = s.nextLine();
-
-         left = 0;
-         right = str.length() - 1;
-
-         while (str.charAt(left) == str.charAt(right) && left < right)
-         {
-            left++;
-            right--;
-         }
-
-         System.out.println();
-
+            
+         
          if (left < right)
+         {
             System.out.println ("That string is NOT a palindrome.");
+        }
          else
+         {
             System.out.println ("That string IS a palindrome.");
+        }
 
          System.out.println();
          System.out.print ("Test another palindrome (y/n)? ");
          another = s.nextLine();
       }
    }
+   
+   public static boolean isPalindrome( String str )
+   {
+       if( str.length() <= 1)
+       {
+           return true;
+        }
+        
+       String substr = str.substring(1, str.length()-1 );
+       
+       return (( str.charAt(0) == str.charAt( str.length()-1)) && isPalindrome(substr));
+    }
 }
---
+++ Practice unit 8/ListMethods.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ListMethods
+{
+   public static ArrayList<Integer> makeList(int n)
+   {
+      ArrayList<Integer> tempList = null;
+      if (n <= 0)  // The smallest list we can make
+      {
+
+
+
+      }
+      else        // All other size lists are created here
+      {
+
+
+
+      }
+      return tempList;
+   }
+}
